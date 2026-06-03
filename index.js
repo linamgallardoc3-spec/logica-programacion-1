cc663f8e9e750e1c99beb2de1d0ba34c55ba3f4f
Lina María Gallardo Corrales <linamgallardoc3@gmail.com>
2026-06-03 13:35:18 -0500

    Ejercicio Resuelto

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const prompt = require("prompt-sync")();
+let mayor;
+let menor;
+let medio;
+let num = [];
+let incremento = 1;
+while (incremento<4) {
+    num.push(parseInt(prompt("Ingrese un número: ")));
+    incremento++;
+}
+const iguales = array => array.every(dato => dato===array[0]);
+
+if(iguales(num)){
+    console.log("Los numeros ingresados son iguales", num)
+}else{
+    menor = Math.min(...num);
+    mayor = Math.max(...num);
+    const indice = num.findIndex(num => num === mayor);
+    num.splice(indice,1);
+    const indice1 = num.findIndex(num => num === menor);
+    num.splice(indice1,1);
+    medio = num[0]
+    console.log(`
+    Los números de mayor a menor son:
+        ${mayor}
+        ${medio}
+        ${menor}`);
+    console.log(`
+    Los números de menor a mayor son:
+        ${menor}
+        ${medio}
+        ${mayor}`);
+}
+
